75dab57b7ede224001d3d54e9918956d52aae45a
randa yahya <randaalrayan96@gmial.com>
2020-07-04 21:46:47 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(time){
+  let newTime=time.split(':');
+  let HH=parseInt(newTime[0]);
+  let MM=parseInt(newTime[1]);
+  console.log(HH);
+  console.log(HH);
+  if(HH<12 && MM>=0){
+    return 'Good Morning';
+  }
+  else if(HH>12 && HH<17 && MM>=0 ){
+    return 'Good Afternoon';
+  }
+  else{
+    return 'Good Evening';
+    console.log(MM);
+  }
 
+}
+
+function displayMessage(str){
+  document.querySelector('#greeting').innerText=str;
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
